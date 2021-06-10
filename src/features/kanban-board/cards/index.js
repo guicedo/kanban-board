74b3f3cdb17336a08f8032881499603b752c6610
guicedo <guilherme01macedo@gmail.com>
2021-06-10 20:55:21 -0300

    basee of the card component  and the css page created

---
+++ src/features/kanban-board/cards/index.js
@@ -0,0 +1,11 @@
+// import { connect } from 'react-redux';
+
+import Cards from './cards';
+/*
+const mapStateToProps = ({ analiseGeral }) => ({
+  overview: analiseGeral.overview.overview,
+});*/
+
+// export default connect(mapStateToProps, null)(Cards);
+
+export default Cards;
--- src/features/kanban-board/kanbanBoardPage.jsx
+++ src/features/kanban-board/kanbanBoardPage.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import Title from '../commonComponents/title';
+import Cards from './cards';
+
 import './kanbanBoardPage.scss';
+
+const cards = [{
+  title: "📝  To Do",
+  backgroundColor: "#5CC4FF",
+  tasks: [],
+},
+{
+  title: "💻  In Progress",
+  backgroundColor: "#945AD1",
+  tasks: [],
+},
+{
+  title: "🚀  Done",
+  backgroundColor: "#59D090",
+  tasks: [],
+}];
+
 
 const KanbanBoardPage = () => (
   <div className="kanban-board-page">
     <Title title="Kanban do projeto"/>
+    <div className="kanban-board-page-content">
+      {(cards || []).map((card) => (
+        <Cards
+          card={card}
+        />
+      ))}
+    </div>
   </div>
 );
-
 
 export default KanbanBoardPage;
